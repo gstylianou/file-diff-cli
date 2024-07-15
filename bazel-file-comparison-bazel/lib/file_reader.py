2726255055fc0519b01d6c3c0b28b0562a8ce6b7
Georgios Stylianou <gstylianou@users.noreply.github.com>
2024-07-15 15:23:00 +0200

    remove unnecessary files

--- bazel-file-comparison-bazel/lib/file_reader.py
+++
@@ -1,8 +0,0 @@
-class FileReader:
-    def __init__(self, path):
-        self.path = path
-
-    def read(self):
-        with open(self.path) as hello_file:
-            hello_file_content = hello_file.readlines()
-            return hello_file_content
